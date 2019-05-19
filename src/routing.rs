91c3bea986dd88806d223044dc11f6a22b00c75b
Lúcás Meier <cronokirby@gmail.com>
2019-05-19 20:34:44 +0200

    Add functions for handling successful and failed pings
    This also switches to using VecDequeue since so many operations
    involve the front and back elements exclusively.

--- src/routing.rs
+++ src/routing.rs
@@ -1,18 +1,22 @@
+use std::collections::VecDeque;
+
 enum KBucketInsert<T> {
     Inserted,
-    Ping(T)
+    Ping(T),
 }
 
 struct KBucket<T> {
     max_size: usize,
-    data: Vec<T>,
+    waiting: Option<T>,
+    data: VecDeque<T>,
 }
 
 impl<T: Clone + PartialEq> KBucket<T> {
     fn new(max_size: usize) -> Self {
         KBucket {
             max_size,
-            data: Vec::with_capacity(max_size),
+            waiting: None,
+            data: VecDeque::with_capacity(max_size),
         }
     }
 
@@ -22,10 +26,27 @@ impl<T: Clone + PartialEq> KBucket<T> {
             self.data.remove(index);
         }
         if self.data.len() < self.max_size {
-            self.data.push(item);
+            self.data.push_back(item);
             return KBucketInsert::Inserted;
         } else {
+            self.waiting = Some(item);
             return KBucketInsert::Ping(self.data[0].clone());
+        }
+    }
+
+    fn successful_ping(&mut self) {
+        self.waiting = None;
+        if let Some(item) = self.data.pop_front() {
+            self.data.push_back(item);
+        }
+    }
+
+    fn failed_ping(&mut self) {
+        // Normally this should only be called if we requested a ping,
+        // and there's an item waiting, but we can just do nothing instead.
+        if let Some(item) = self.waiting.take() {
+            self.data.pop_front();
+            self.data.push_back(item);
         }
     }
 }
