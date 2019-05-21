ce004937c03a70c3c9d4ede55de05bd406f4582a
Lúcás Meier <cronokirby@gmail.com>
2019-05-21 21:51:03 +0200

    WIP: Message Definitions

---
+++ src/messages.rs
@@ -0,0 +1,21 @@
+use crate::base::{BitKey, Node};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct TransactionID(u64);
+
+struct Header {
+    node_id: BitKey,
+    transaction_id: TransactionID
+}
+
+enum RPCPayload {
+    Ping,
+    PingResp,
+    FindValue(String),
+    FindValueResp(String),
+    FindValueNodes(Vec<Node>),
+    FindNode(BitKey),
+    FindNodeResp(Vec<Node>),
+    Store,
+    StoreResp
+}
